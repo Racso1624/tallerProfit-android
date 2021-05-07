5c8586cc20fae7016d9acac44c41c8e74adde000
Racso1624 <fer10_lb@outlook.com>
2021-05-06 23:40:32 -0600

    Se termina el programa

--- LaboratorioCalculadora/app/src/main/java/com/prueba/pruebataller/MainActivity.kt
+++ LaboratorioCalculadora/app/src/main/java/com/prueba/pruebataller/MainActivity.kt
@@ -4,18 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show()
-
         val btnHello: Button = findViewById(R.id.btnHello)
 
-        btnHello.setOnClickListener {
+        btnHello.setOnClickListener{
             val intent: Intent = Intent(this, Calculadora::class.java)
             startActivity(intent)
             finish()
